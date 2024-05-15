8426fad7eecacbadcd694dc4a7eaa39d823fac8e
motty <mlc40622@gmail.com>
2024-05-15 14:41:27 +0300

    main tamplate

--- src/app/features/features.module.ts
+++ src/app/features/features.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainPageComponent } from './main-page/main-page.component';
 
 
 
 @NgModule({
   declarations: [
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
+    MainPageComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
     HeaderComponent,
+    FooterComponent,
+    MainPageComponent,
   ]
 })
 export class FeaturesModule { }
