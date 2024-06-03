58b0c07b99f412c4eaba742a0bb956b3a19331d4
motty <mlc40622@gmail.com>
2024-06-03 13:24:14 +0300

    complete

--- src/app/admin/components/edit-discounts/edit-discounts.component.ts
+++ src/app/admin/components/edit-discounts/edit-discounts.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { EditDiscountsService } from '../../services/edit-discounts.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-edit-discounts',
@@ -6,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrl: './edit-discounts.component.css'
 })
 export class EditDiscountsComponent {
+  discount! : number
 
+  constructor(private discountService : EditDiscountsService, private router : Router){}
+
+  addDiscount(){
+    if(this.discount && this.discount > 0 && this.discount < 100){
+      this.discountService.addDiscountForAllBooks(this.discount)
+      this.router.navigate(['/admin/managePanel'])
+    }
+  }
+  removeDiscount(){
+    this.discountService.removeDiscountFromAllBooks()
+    this.router.navigate(['/admin/managePanel'])
+  }
 }
--- src/app/admin/services/edit-discounts.service.ts
+++ src/app/admin/services/edit-discounts.service.ts
@@ -1,7 +1,20 @@
 import { Injectable } from '@angular/core';
+import { VirtualDB } from '../../core/utils/virtualDB.booksList';
 
 @Injectable()
 export class EditDiscountsService {
 
-  constructor() { }
+  constructor(private booksDB : VirtualDB) { }
+
+  addDiscountForAllBooks(precent : number){
+    for (let book of this.booksDB.books) {
+      book.discountByPrecent = precent
+    }
+  }
+
+  removeDiscountFromAllBooks(){
+    for (let book of this.booksDB.books) {
+      book.discountByPrecent = null
+    }
+  }
 }
